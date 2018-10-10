f8410fbab409ae0a9d0727127b9b24b97aa931da
andacx <zhangwenchao@andacx.com>
2018-10-10 16:25:22 +0800

    提交

---
+++ PdvertisPage/PdvertisPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PdvertisPage
+//
+//  Created by andacx on 2018/10/10.
+//  Copyright © 2018年 andacx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PdvertisPage/PdvertisPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PdvertisPage
+//
+//  Created by andacx on 2018/10/10.
+//  Copyright © 2018年 andacx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
